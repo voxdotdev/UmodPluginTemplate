a9526bd8aa28cf0552818904d802d9f74446f4fb
Vox <voxdotdev@gmail.com>
2021-01-19 12:54:53 -0600

    Add project files.

---
+++ UmodPluginTemplate.cs
@@ -0,0 +1,16 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("Epic Stuff", "Unknown Author", "0.1.0")]
+    [Description("Makes epic stuff happen")]
+    class UmodPluginTemplate : CovalencePlugin
+    {
+        private void Init()
+        {
+            Puts("A baby plugin is born!");
+        }
+
+        // The rest of the code magic
+
+        // TODO (you): Make more epic stuff
+    }
+}
