780f5b082173e387bd1c43c0b2ffab0ddb7af065
Vox <voxdotdev@gmail.com>
2021-01-19 13:01:38 -0600

    foo

--- UmodPluginTemplate.cs
+++ UmodPluginTemplate.cs
@@ -2,15 +2,35 @@
 {
     [Info("Epic Stuff", "Unknown Author", "0.1.0")]
     [Description("Makes epic stuff happen")]
+   
+    #region Main
     class UmodPluginTemplate : CovalencePlugin
     {
+        #region Initialization
+
         private void Init()
         {
             Puts("A baby plugin is born!");
         }
+        
+        #endregion
 
         // The rest of the code magic
 
         // TODO (you): Make more epic stuff
     }
+    #endregion
+
+    #region Config
+    #endregion
+
+    #region Localization
+    #endregion
+
+    #region Data Files
+    #endregion
+
+    #region Hooks
+    #endregion
+
 }
